10d47a4ece84957c3e3555783f5f466ae7679589
Ztj <ztj1993@gmail.com>
2019-08-16 15:13:56 +0800

    Update docsify_entrypoint.sh

--- docsify_entrypoint.sh
+++ docsify_entrypoint.sh
@@ -2,11 +2,8 @@
 
 set -e
 
-docs_dir=/docsify.docs
-
-if [[ ! -f /docsify_index.html ]]; then
-    wget -O /docsify_index.html https://raw.githubusercontent.com/ztj1993/files/master/docsify_index.html
-fi
+docs_dir=${DOCSIFY_DOCS_DIR:-/docsify.docs}
+echo "docs dir: ${docs_dir}"
 
 for doc_dir in $(ls ${docs_dir}); do
     if [[ ! -d ${docs_dir}/${doc_dir} ]]; then
@@ -14,6 +11,7 @@ for doc_dir in $(ls ${docs_dir}); do
     fi
     if [[ ! -f ${docs_dir}/${doc_dir}/index.html ]]; then
         cp /docsify_index.html ${docs_dir}/${doc_dir}/index.html
+        echo "copy file: ${docs_dir}/${doc_dir}/index.html"
     fi
     if [[ -f ${docs_dir}/${doc_dir}/.docsify/_coverpage.md ]]; then
         sed -i "s/coverpage\:.*,/coverpage: true,/" ${docs_dir}/${doc_dir}/index.html
