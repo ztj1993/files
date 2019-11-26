37a00f6bcd27451df92cc4146b977e3a7495e25c
Ztj <ztj1993@gmail.com>
2019-11-26 13:52:05 +0800

    Update web_ssl.sh

--- shell/web_ssl.sh
+++ shell/web_ssl.sh
@@ -9,23 +9,21 @@
 #     wget https://raw.githubusercontent.com/ztj1993/files/master/shell/web_ssl.sh
 #     chmod +x web_ssl.sh
 #     sudo ./web_ssl.sh
+#     or
+#     curl -fsSL https://raw.githubusercontent.com/ztj1993/files/master/shell/web_ssl.sh | bash
 ###############
 
 set -e
 
-domain=${1}
-skip_confirm=${2:-no}
+domain=${domain:-${1:-domain}}
+ssl_dir=${ssl_dir:-${2:-/etc/nginx/ssl}}
 
-[[ -z ${domain} ]] && read -p ">>> Please enter the domain: " read_domain
-[[ -z ${domain} ]] && [[ -z ${read_domain} ]] && echo ">>> Please enter the correct  domain." && exit 1
-[[ -n ${read_domain} ]] && domain=${read_domain}
+[[ -z ${domain} ]] && echo ">>> Not domain." && exit 1
 
-echo ">>> Domain=${domain}"
+ssl_dir=${ssl_dir}/${domain}
 
-[[ "${skip_confirm}" != "yes" ]] && read -p ">>> Please confirm(yes/no): " read_confirm
-[[ "${skip_confirm}" != "yes" ]] && [[ ${read_confirm} != "yes" ]] && echo ">>> Not confirmed." && exit 1
-
-ssl_dir=/etc/nginx/ssl/${domain}
+echo ">>> domain=${domain}"
+echo ">>> ssl_dir=${ssl_dir}"
 
 docker exec web_acme --issue -d ${domain} -d *.${domain} --dns dns_ali
 
